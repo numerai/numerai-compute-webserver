90d4d2aa6155b379d5fcffe89067b27edb372237
Jason Paryani <github@jparyani.com>
2019-06-01 11:35:46 -0700

    Fix for allowing POST method

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/submit')
+@app.route('/submit', methods=['GET', 'POST'])
 def submit():
     predict.predict_and_submit()
     return jsonify({"status": "ok"})
 
 
-@app.route('/train')
+@app.route('/train', methods=['GET', 'POST'])
 def train():
     t.train()
     return jsonify({"status": "ok"})
